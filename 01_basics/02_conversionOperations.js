d0dc96e0f21dbd6927ad15a4bc397a48cd0a3504
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-02-26 15:20:24 +0000

    conversion operations done

--- 01_basics/02_conversionOperations.js
+++ 01_basics/02_conversionOperations.js
@@ -1,8 +1,24 @@
-let score = null
+//let score = "33"
 
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//let someNumber = 55
+
+
+
+//let stringNumber = String(someNumber)
+//
+
+// ---------------*****************Operations*******************-----------------------
+
+
+
+console.log(+true)
+let gameCounter = "100"
+++gameCounter;
+console.log(gameCounter)
