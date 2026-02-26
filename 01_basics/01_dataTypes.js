5bf6b2c17d38a5d4452b4577316310b5c85210e4
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-02-26 09:10:56 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3); // code readability should be high
+
+console.log("Hitesh");
+
+let name ="Naveen"
+let age = 25
+let isloggedIn=false
+let state=null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.table([typeof name, typeof age, typeof isloggedIn, typeof state])
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 663681
+let accountEmail = "naveen@example.com"
+let accountPassword = "98765"
+accountcity = "Mumbai"
+
+let accountState;
+
+// *Prefer not to use var
+    // because of issue in block scope and functional scope
+
+
+accountEmail ="tiwari@hh.com"
+accountPassword ="23456"
+accountCity ="Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
