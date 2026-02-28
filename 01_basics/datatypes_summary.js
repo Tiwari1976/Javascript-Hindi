47973eaa3af18f1d0b65b4cc1625035a9d59fd1b
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-02-28 09:13:51 +0000

    datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId); // false
+
+const bigNumber = 3456543576654356754n;
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Shaktiman", "Naagraj", "Doga"];
+
+let myObj = {
+  name: "Naveen",
+  age: 25,
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+};
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
