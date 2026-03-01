a0dcf5937eb1d1d6fd1696e63f0e45ad390c5d6c
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-03-01 08:13:57 +0000

    maths and nums done

---
+++ 01_basics/05_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score =300
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const otherNumber = 23.5678
+
+//console.log(otherNumber.toPrecision(3))
+
+const hundred = 1000000
+//console.log(hundred.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.7))
+// console.log(Math.ceil(5.4))
+// console.log(Math.floor(5.7))
+
+// console.log(Math.min(3,6,8,2))
+// console.log(Math.max(3,6,8,2))
+
+// console.log(Math.random())
+// console.log(Math.random()*10)
+//onsole.log((Math.random()*10) + 1)
+
+const min =20
+const max =30
+
+const randomNum =Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum)
+
