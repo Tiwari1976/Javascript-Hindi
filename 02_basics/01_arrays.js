e0f8092a9aee966083e4c9e574e3568bfc60913d
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-03-02 08:30:40 +0000

    arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+const myArr = [0,1,2,3,4,5]
+const myHero = ["Spiderman", "Batman"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1])
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+//myArr.unshift(7)
+// myArr.shift()
+
+const newArr = myArr.join()
+console.log(myArr)
+console.log(newArr)
+
+console.log(" A", myArr)
+
+const myn1 = myArr.slice(1, 3)
+console.log(" B", myArr)
+console.log(myn1)
+
+const myn2 =myArr.splice(1, 4)
+console.log(myn2)
