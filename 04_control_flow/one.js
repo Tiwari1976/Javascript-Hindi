6100a6368a5b8eea4103be267b0f97a40649758d
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-03-07 08:38:26 +0000

    control flow all donw

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,23 @@
-const temperature = 41;
+// const temperature = 41;
 
-if (temperature < 40){
-    console.log("less than 50")
-} else {
-    console.log("temperature is greater than 50")
+// if (temperature < 40){
+//     console.log("less than 50")
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+const userLoggedInFromGoogle  =true
+const userLoggedInEmail= true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow user to buy")
+}
+
+
+if(userLoggedInFromGoogle || userLoggedInEmail ){
+    console.log("User Logged in")
 }
 
