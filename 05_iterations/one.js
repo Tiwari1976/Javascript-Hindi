6f57ac4259e5dba0e7215468722d2ecf91a400a9
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-03-07 09:38:30 +0000

    iteration one

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+for(let i=0; i<=10; i++){
+    const element = i
+    if (element == 5){
+       // console.log("5 is best number")
+    }
+   // console.log(element)
+}
+
+// for(i=0; i <=10; i++){
+//     console.log(`Outer loop value: ${i}`)
+//     for(j=0; j<=10; j++){
+        //console.log(`Inner loop value ${j} and outer loop ${i} `)
+        //console.log(i + '*' + j + ' = ' + i*j )
+   // }
+    
+//}
+
+let myArray = ["flash", "batsman", "spiderman"]
+for(let i=0; i<myArray.length; i++){
+    const element = myArray[i];
+console.log(element)
+}
