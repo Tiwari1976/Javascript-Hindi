ab2beb47d2829a025ac0629a79f03a81552975a1
Nabin Tiwari <145090455+Tiwari1976@users.noreply.github.com>
2026-03-08 07:00:47 +0000

    iterations all done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -18,5 +18,14 @@ for(let i=0; i<=10; i++){
 let myArray = ["flash", "batsman", "spiderman"]
 for(let i=0; i<myArray.length; i++){
     const element = myArray[i];
-console.log(element)
+//console.log(element)
 }
+
+for(let index=1; index <=20; index++){
+   if(index == 5){
+      console.log(`Detected 5`)
+      continue
+   }
+   console.log(`Value of i is ${index}`);
+}
+
